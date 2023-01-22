0cb395bac13cab227dae6292b62660f343292a2e
Carmen <54728377+crfews@users.noreply.github.com>
2023-01-22 11:49:58 -0500

    move winder functions to .cpp

--- openmandrel/axisymmetric_winder.cpp
+++ openmandrel/axisymmetric_winder.cpp
@@ -5,9 +5,35 @@
 
 
 
-Vec2 AxisymmetricWinder::calcWoundMandrelSegment(AxisymmetricMandrelSegment *segment) {
-    Vec2 derivatives = *segment.CylindricalDerivatives(1);
+AxisymmetricWinder::AxisymmetricWinder(double Width, int NumberOfPoints=15) {
+    this->NumberOfPoints = NumberOfPoints;
+    BandWidth = Width;
+}
 
-    return e;
 
+double AxisymmetricWinder::getWindAngle(int T0, int T1) {
+    // solve nonlinear differential equation
+    // get magnitude of t vector and T vector
+    // dot product to obtain angle
+    return -1;
+}
+
+double AxisymmetricWinder::getMinimumRadius(int T0, int T1) {
+    // get coords at T0 and T1
+    // calculate distance between coordinates on the 
+}
+
+double AxisymmetricWinder::getPitch(double WindingAngle, double MandrelDiameter) {
+    // returns the Pitch
+    return (PI*MandrelDiameter)/(tan(WindingAngle));
+}
+
+void AxisymmetricWinder::setWindingAngle(double Angle) {
+    WindingAngle = Angle;
+}
+
+std::complex<double> AxisymmetricWinder::getRequiredCircuits(AxisymmetricMandrelSegment &segment, int parameter) {
+    std::complex<double> NumberOfCircuits = 2 * PI * segment.Radius(parameter) * cos(WindingAngle) / BandWidth;
+    //double Diameter = complex(2) *segment.Radius(parameter);
+    return NumberOfCircuits;
 }
